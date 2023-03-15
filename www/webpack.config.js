276c98740050f6210aaac13e8e3e276718400e31
Young wook Jeon <ela87@naver.com>
2023-03-16 02:57:54 +0900

    Initial commit

---
+++ www/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './bootstrap.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bootstrap.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: './index.html', to: './' }],
+    }),
+  ],
+};
+
+// module.exports = {
+//   entry: './index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'public'),
+//     filename: 'index.js',
+//   },
+//   mode: 'development',
+//   plugins: [
+//     new CopyWebpackPlugin({
+//       patterns: [{ from: './index.html', to: './' }],
+//     }),
+//   ],
+// };
