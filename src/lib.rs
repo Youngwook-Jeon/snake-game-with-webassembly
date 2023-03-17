bb8ed0ce5b0ff684201f76075c0f316ebb29ca5c
Young wook Jeon <ela87@naver.com>
2023-03-17 19:24:58 +0900

    Draw the world

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,17 @@ static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub struct World {
-    pub width: usize,
+    width: usize,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
         World { width: 8 }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
     }
 }
 
